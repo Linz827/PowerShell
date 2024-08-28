f3c0b29d0865f7f5730ca33902f1afd3b6221b88
Linz827 <67765434+Linz827@users.noreply.github.com>
2024-08-28 13:16:53 +1200

    Add files via upload

---
+++ add-odbc-connections.ps1
@@ -0,0 +1,3 @@
+﻿Add-OdbcDsn -Name ldms7read -DriverName "SQL Server" -DsnType User -Platform 32-bit -SetPropertyValue @("Server=GBCSQL5.cis.fb.net.nz","Trusted_Connection=Yes", "Database=GBCLDMS7")
+
+Add-OdbcDsn -Name ldms7write -DriverName "SQL Server" -DsnType User -Platform 32-bit -SetPropertyValue @("Server=GBCSQL5.cis.fb.net.nz", "Trusted_Connection=Yes", "Database=GBCLDMS7")
